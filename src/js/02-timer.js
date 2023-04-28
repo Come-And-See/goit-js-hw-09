87ac1ee4db77f20b4760cf0e0b8ad4e98eb06e59
ComeAndSee-CAS <comeandsee.seo@gmail.com>
2023-04-28 16:06:33 +0300

    UP

---
+++ src/js/02-timer.js
@@ -0,0 +1,75 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] >= Date.now()) {
+      refs.start.removeAttribute('disabled');
+      dateUser = new Date(refs.input.value);
+    } else {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  start: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.start.setAttribute('disabled', 'disabled');
+refs.start.addEventListener('click', timerStart);
+
+function timerStart() {
+  const intervalId = setInterval(() => {
+    upTimerVisual(convertMs(dateUser - Date.now()));
+    if (dateUser - Date.now() < 1000) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
+
+function upTimerVisual({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,71 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount.
+// Під час кожного виклику передай їй номер промісу(position), що створюється, і затримку, враховуючи першу затримку(delay), введену користувачем, і крок(step).
+
+// Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу.
+// Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів.
+// Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
+
+import Notiflix from 'notiflix';
+
+const refs = {
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  submit: document.querySelector('button[type="submit"]'),
+};
+
+refs.submit.addEventListener('click', launchPromise);
+
+function launchPromise(e) {
+  e.preventDefault();
+
+  const DELAY = refs.delay.value;
+  const STEP = refs.step.value;
+  const AMOUNT = refs.amount.value;
+
+  let position = 0;
+  let delay = +DELAY;
+
+  setTimeout(() => {
+    const intervalID = setInterval(() => {
+      position += 1;
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+      delay += +STEP;
+      if (position === +AMOUNT) {
+        clearInterval(intervalID);
+      }
+    }, STEP);
+  }, DELAY);
 }
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+      resolve({ position, delay });
+    } else {
+      // Reject
+      reject({ position, delay });
+    }
+  });
+  return promise;
+}
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
